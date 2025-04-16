b6bae5c008328be88fe7e3595e4fe6d65dd16a66
palak porwal <99019333+39palakved@users.noreply.github.com>
2025-04-16 16:37:52 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 connectToMongo(); // Pehle MongoDB connect karo
 
 const app = express(); // Phir Express app initialize karo
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
